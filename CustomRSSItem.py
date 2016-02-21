e3600b0baea2251af8ea16e7e1612abcd4b0f15f
Toby Kavukattu <kavukattu@gmail.com>
2016-02-20 18:36:00 -0600

    Made custom items dynamic

--- CustomRSSItem.py
+++ CustomRSSItem.py
@@ -12,48 +12,12 @@ class CustomRSSItem(PyRSS2Gen.RSSItem):
                  guid = None,    # a unique string
                  pubDate = None, # a datetime
                  source = None,  # a Source
-                 
-                 #custom hipc rss fields
-                 start_date=None,
-                 end_date=None,
-                 start_time=None,
-                 end_time=None,
-                 venue=None,
-                 venue_address=None,
-                 venue_city=None,
-                 venue_state=None,
-                 event_type=None,
-                 event_website=None,
-                 event_image=None,
-                 payment=None
+                 customItems = None, #custom rss fields
                  ):
         
         PyRSS2Gen.RSSItem.__init__(self, title,link,description,author,categories,comments,enclosure,guid,pubDate,source)
-      
-        self.start_date = start_date
-        self.end_date = end_date
-        self.start_time = start_time
-        self.end_time = end_time
-        self.venue = venue
-        self.venue_address = venue_address
-        self.venue_city = venue_city
-        self.venue_state = venue_state
-        self.event_type = event_type
-        self.event_website = event_website
-        self.event_image = event_image
-        self.payment = payment
-        
+        self.customItems = customItems
         
     def publish_extensions(self, handler):
-        PyRSS2Gen._opt_element(handler, "start_date", self.start_date)
-        PyRSS2Gen._opt_element(handler, "end_date", self.end_date)
-        PyRSS2Gen._opt_element(handler, "start_time", self.start_time)
-        PyRSS2Gen._opt_element(handler, "end_time", self.end_time)
-        PyRSS2Gen._opt_element(handler, "venue", self.venue)
-        PyRSS2Gen._opt_element(handler, "venue_address", self.venue_address)
-        PyRSS2Gen._opt_element(handler, "venue_city", self.venue_city)
-        PyRSS2Gen._opt_element(handler, "venue_state", self.venue_state)
-        PyRSS2Gen._opt_element(handler, "event_type", self.event_type)
-        PyRSS2Gen._opt_element(handler, "event_website", self.event_website)
-        PyRSS2Gen._opt_element(handler, "event_image", self.event_image)
-        PyRSS2Gen._opt_element(handler, "payment", self.payment)
+        for key, value in self.customItems.items():
+            PyRSS2Gen._opt_element(handler, key, value)
